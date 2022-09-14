d12e3025c22b910c8ecdad1041f3f7d8ed3da938
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-09-14 20:01:51 +0500

    deleted unneccessary comments

--- src/components/MyResponsiveGrid/MyResponsiveGrid.jsx
+++ src/components/MyResponsiveGrid/MyResponsiveGrid.jsx
@@ -1,30 +1,3 @@
-// import React from "react";
-
-// import { Responsive as ResponsiveGridLayout } from "react-grid-layout";
-
-// export const MyResponsiveGrid = () => {
-//   const layouts = [
-//     { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-//     { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-//     { i: "c", x: 4, y: 0, w: 1, h: 2 },
-//   ];
-
-//   return (
-//     <ResponsiveGridLayout
-//       className="layout"
-//       layouts={layouts}
-//       width={1200}
-//       breakpoint={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-//       cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-//       style={{ background: "yellow" }}
-//     >
-//       <div key={"1"} style={{ background: "green" }}>1</div>
-//       <div key={"2"} style={{ background: "green" }}>2</div>
-//       <div key={"3"} style={{ background: "green" }}>3</div>
-//     </ResponsiveGridLayout>
-//   )
-// }
-
 import { Responsive, WidthProvider } from "react-grid-layout";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
